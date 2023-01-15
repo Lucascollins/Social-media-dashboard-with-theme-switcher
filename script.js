89684ee7bee4847cbb1b86f12a057f3c601d3a57
Lucascollins <lucascollins506@gmail.com>
2023-01-15 11:46:34 -0300

    toogle button mode resolving bug hover

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 'use strict'
 
 
+
 function Mode(){
     var input = document.getElementById('bttn')
     var back = document.querySelector('.back')
     var cards = document.querySelectorAll(".social-grid, .overview-grid")
-    var texts = document.querySelectorAll(".header-text, h1")
+    var texts = document.querySelectorAll(".header-text, h1,.header")
     var subText = document.querySelectorAll('p')
+
+
     if(input.checked == true){
         document.body.style.background = "hsl(0, 0%, 100%)";
-        back.style.background = "hsl(225, 100%, 98%)";
-        cards.forEach(element => {
-            element.style.background = 'hsl(227, 47%, 96%)'
+        back.style.background = "hsl(220, 100%, 98%)";
+        cards.forEach(e => {
+            e.classList.toggle('active')
         });
-        texts.forEach(element=>{
-            element.style.color = 'hsl(230, 17%, 14%)'
+        texts.forEach(e=>{
+            e.classList.toggle('active')
         });
-        subText.forEach(element=>{
-            element.style.color = 'hsl(228, 12%, 44%)'
+        subText.forEach(e=>{
+            e.classList.toggle('active')
         })
         
     }else{
         document.body.style.background = "hsl(230, 17%, 14%)";
         back.style.background = "hsl(232, 19%, 15%)";
-        cards.forEach(element => {
-            element.style.background = 'hsl(228, 28%, 20%)'
+        cards.forEach(e => {
+            e.classList.toggle('active')
         });
-        texts.forEach(element=>{
-            element.style.color = 'hsl(0, 0%, 100%)'
+        texts.forEach(e=>{
+            e.classList.toggle('active')
         });
-        subText.forEach(element=>{
-            element.style.color = 'hsl(228, 34%, 66%)'
+        subText.forEach(e=>{
+            e.classList.toggle('active')
         })
     }
 }
